9c7d4abdc3e239b4fb3bc293f2cb2b2d99eb6adc
Jack Gitter <jack.a.gitter@gmail.com>
2026-02-13 14:47:05 -0500

    ok

--- src/coordinator/transaction-coordinator.ts
+++ src/coordinator/transaction-coordinator.ts
@@ -8,14 +8,21 @@ export class TransactionCoordinator {
 	constructor(private personsService: ICoordinatableService, private addressService: ICoordinatableService, private coordinatorRepository: Repository<CoordinatorLog>) {}
 
 	async begin() {
+		// WAL
 		const txId = randomUUID()
 		const log = new CoordinatorLog(txId)
 		await this.coordinatorRepository.save(log)
+
+		// PHASE 1
 		const personResponse = await this.personsService.prepare(txId)
 		const addressResponse = await this.addressService.prepare(txId)
+
+		// WAL
 		log.status1 = personResponse ? STATUS.SUCCESS : STATUS.FAILURE
 		log.status2 = addressResponse ? STATUS.SUCCESS : STATUS.FAILURE
 		await this.coordinatorRepository.save(log)
+
+		// PHASE 2
 		if (personResponse && addressResponse) {
 			await this.commit(txId)
 		} else {
@@ -23,11 +30,13 @@ export class TransactionCoordinator {
 		}
 	}
 
+	// keep trying here if we can a connection error. If we get an error that no txid exists, then stop. If we get any other error, that is bad
 	async rollback(txid: UUID) {
 		await this.personsService.rollback(txid)
 		await this.addressService.rollback(txid)
 	}
 
+	// keep retrying here if we get a connection error. If we get an error that no txid exists, then stop. If we get any other error, that is bad
 	async commit(txid: UUID) {
 		await this.personsService.commit(txid)
 		await this.addressService.commit(txid)
