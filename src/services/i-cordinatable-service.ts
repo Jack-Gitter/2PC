38678597390e40f461aae4e95a15644632778415
Jack Gitter <jack.a.gitter@gmail.com>
2026-02-13 15:17:32 -0500

    ok

--- src/services/i-cordinatable-service.ts
+++ src/services/i-cordinatable-service.ts
@@ -3,6 +3,8 @@ import {UUID} from 'crypto'
 export interface ICoordinatableService {
 	// this function should be idempotent (check if the tx already exists or not before creating)
 	prepare(txid: UUID): Promise<boolean>
+	// this function should be idempotent (check if the tx already exists or not before committing)
 	commit(txid: UUID)
+	// this function should be idempotent (check if the tx already exists or not before rolling back)
 	rollback(txid: UUID)
 }
