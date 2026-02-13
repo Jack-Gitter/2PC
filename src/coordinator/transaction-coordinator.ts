7d1ba0743fced4f6a937e1e110515533a42ca66f
Jack Gitter <jack.a.gitter@gmail.com>
2026-02-13 14:59:00 -0500

    ok

--- src/coordinator/transaction-coordinator.ts
+++ src/coordinator/transaction-coordinator.ts
@@ -13,16 +13,23 @@ export class TransactionCoordinator {
 		const log = new CoordinatorLog(txId)
 		await this.coordinatorRepository.save(log)
 
-		// PHASE 1
-		const personResponse = await this.personsService.prepare(txId)
-		const addressResponse = await this.addressService.prepare(txId)
+		const { personResponse, addressResponse } = await this.phase1(txId)
 
 		// WAL
 		log.status1 = personResponse ? STATUS.SUCCESS : STATUS.FAILURE
 		log.status2 = addressResponse ? STATUS.SUCCESS : STATUS.FAILURE
 		await this.coordinatorRepository.save(log)
 
-		// PHASE 2
+		await this.phase2(txId, personResponse, addressResponse)
+	}
+
+	private async phase1(txId: UUID): Promise<{personResponse: boolean, addressResponse: boolean}> {
+		const personResponse = await this.personsService.prepare(txId)
+		const addressResponse = await this.addressService.prepare(txId)
+		return {personResponse, addressResponse}
+	}
+
+	private async phase2(txId: UUID, personResponse: boolean, addressResponse: boolean) {
 		if (personResponse && addressResponse) {
 			await this.commit(txId)
 		} else {
@@ -34,15 +41,41 @@ export class TransactionCoordinator {
 	async rollback(txid: UUID) {
 		await this.personsService.rollback(txid)
 		await this.addressService.rollback(txid)
+		await this.coordinatorRepository.delete({ transactionId: txid });
 	}
 
 	// keep retrying here if we get a connection error. If we get an error that no txid exists, then stop. If we get any other error, that is bad
 	async commit(txid: UUID) {
 		await this.personsService.commit(txid)
 		await this.addressService.commit(txid)
+		await this.coordinatorRepository.delete({ transactionId: txid });
 	}
 
-	recover() {
-		// load transactions from db, create prepared transactions if no status, and commit/rollback as necessary if status exists
+	async recover() {
+		const incompleteTxs = await this.coordinatorRepository.find();
+
+		for (const log of incompleteTxs) {
+			try {
+				const phase1Completed = log.status1 && log.status2
+				
+				if (phase1Completed) {
+					const personSuccess = log.status1 === STATUS.SUCCESS;
+					const addressSuccess = log.status2 === STATUS.SUCCESS;
+					
+					await this.phase2(log.transactionId, personSuccess, addressSuccess);
+					
+				} else {
+					const { personResponse, addressResponse } = await this.phase1(log.transactionId);
+					
+					log.status1 = personResponse ? STATUS.SUCCESS : STATUS.FAILURE;
+					log.status2 = addressResponse ? STATUS.SUCCESS : STATUS.FAILURE;
+					await this.coordinatorRepository.save(log);
+					
+					await this.phase2(log.transactionId, personResponse, addressResponse);
+				}
+			} catch (error) {
+				console.error(`Failed to recover transaction ${log.transactionId}:`, error);
+			}
+		}
 	}
 }
