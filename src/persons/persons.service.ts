44794f2e76516399313b3bd7fa8f385c81579f6a
Jack Gitter <jack.a.gitter@gmail.com>
2026-02-15 10:19:02 -0500

    idempotent and db implementation

--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -6,16 +6,78 @@ export class PersonsService implements ICoordinatableService {
 
 	constructor(private datasource: DataSource) {}
 
-	// make this idemptontent, look for txid before doing any action
     async prepare(txid: UUID): Promise<boolean> {
-        throw new Error("Method not implemented.");
+
+		const txExists = await this.txExists(txid)
+		
+		// if txExists, return true
+
+		const query = `
+		BEGIN TRANSACTION;
+
+		UPDATE 
+			PERSONS 
+		SET 
+			FIRSTNAME = 'New Firstname' 
+		WHERE 
+			id = 1;
+
+		PREPARE TRANSACTION $1;
+		`
+
+		const result = await this.datasource.query(query, [txid])
+
+		console.log(result)
+
+		return false
+    }
+
+    async commit(txid: UUID) {
+
+		const txExists = await this.txExists(txid)
+		
+		// if it doesnt exist, return true
+
+		const query = `COMMIT PREPARED $1`
+
+		const result = await this.datasource.query(query, [txid])
+
+		console.log(result)
+
+		return result
+
     }
 	// make this idemptontent, look for txid before doing any action
-    commit(txid: UUID) {
-        throw new Error("Method not implemented.");
+    async rollback(txid: UUID) {
+		const txExists = await this.txExists(txid)
+		
+		// return true if tx does not exist
+		
+		const query = `ROLLBACK PREPARED $1`
+
+		const result = await this.datasource.query(query, [txid])
+
+		console.log(result)
+
+		return result
+
     }
-	// make this idemptontent, look for txid before doing any action
-    rollback(txid: UUID) {
-        throw new Error("Method not implemented.");
-    }
+
+	private async txExists(txid: UUID) {
+
+		const query = `
+			SELECT EXISTS (
+			  SELECT 1
+			  FROM pg_prepared_xacts
+			  WHERE gid = $1 
+			);
+		`
+
+		const result = await this.datasource.query(query, [txid])
+
+		console.log(result)
+
+		return result
+
+	}
 }
